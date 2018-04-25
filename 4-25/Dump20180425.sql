74c937be7368e6c4fefc78600d905ff16d6bf800
DenmarkApilada123 <2160705@slu.edu.ph>
2018-04-25 18:46:36 +0800

    SQL FILE

---
+++ 4-25/Dump20180425.sql
@@ -0,0 +1,147 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: car
+-- ------------------------------------------------------
+-- Server version	5.7.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cars` (
+  `CarID` int(255) NOT NULL AUTO_INCREMENT,
+  `BrandName` varchar(255) NOT NULL,
+  `Model` varchar(255) NOT NULL,
+  `Brand` varchar(255) NOT NULL,
+  `Features` varchar(255) NOT NULL,
+  `TimesRented` int(11) NOT NULL,
+  `Rate` int(11) NOT NULL,
+  `Status` varchar(255) NOT NULL,
+  PRIMARY KEY (`CarID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cars`
+--
+
+LOCK TABLES `cars` WRITE;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `CustomerID` int(255) NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(255) NOT NULL,
+  `LasName` varchar(255) NOT NULL,
+  `Gender` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `PhoneNumber` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  PRIMARY KEY (`CustomerID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `serviceprovider`
+--
+
+DROP TABLE IF EXISTS `serviceprovider`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `serviceprovider` (
+  `SPID` int(255) NOT NULL AUTO_INCREMENT,
+  `BusinessEntity` varchar(255) NOT NULL,
+  `BasedIn` varchar(255) NOT NULL,
+  `PhoneNumer` varchar(255) NOT NULL,
+  `CompanyName` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `BankAccName` varchar(255) NOT NULL,
+  `BankAccNum` varchar(255) NOT NULL,
+  `Revenue` int(11) NOT NULL,
+  PRIMARY KEY (`SPID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `serviceprovider`
+--
+
+LOCK TABLES `serviceprovider` WRITE;
+/*!40000 ALTER TABLE `serviceprovider` DISABLE KEYS */;
+/*!40000 ALTER TABLE `serviceprovider` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction` (
+  `TID` int(255) NOT NULL AUTO_INCREMENT,
+  `SPID` int(255) NOT NULL,
+  `CustomerID` int(255) NOT NULL,
+  `CarID` int(255) NOT NULL,
+  `Status` varchar(255) NOT NULL,
+  `Duration` varchar(255) NOT NULL,
+  `ReturnPoint` varchar(255) NOT NULL,
+  `TotalPrice` int(11) DEFAULT NULL,
+  PRIMARY KEY (`TID`),
+  KEY `Transaction_fk0` (`SPID`),
+  KEY `Transaction_fk1` (`CustomerID`),
+  KEY `Transaction_fk2` (`CarID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-25 18:44:38
